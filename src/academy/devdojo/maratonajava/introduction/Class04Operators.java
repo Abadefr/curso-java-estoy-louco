2848282ac8603289919ba8949677d634d77d6bb7
franklinux <universomacroexpande@gmail.com>
2024-10-08 14:53:43 -0300

    Starting to understand how to use comparisom in java

--- src/academy/devdojo/maratonajava/introduction/Class04Operators.java
+++ src/academy/devdojo/maratonajava/introduction/Class04Operators.java
@@ -8,12 +8,27 @@ public class Class04Operators {
         int number02 = 20;
         int plus = number01 + number02;
         int minus = number01 - number02;
-        int division = number01 / number02;
-        int multiplication = number01 * number02;
+        double division = number01 / (double) number02;
+        int multiplication = number01 * number02; int percentage = number01 % number02;
         int result = number01 - number02;
-        System.out.println("Value :"+ (number01 + number02));
-        System.out.println("Value :"+ (number01 - number02));
-        System.out.println("Value :"+ (number01 / (double) number02));
-        System.out.println("Value :"+ (number01 * number02));
+        System.out.println("Value :"+ plus);
+        System.out.println("Value :"+ minus);
+        System.out.println("Value :"+ division);
+        System.out.println("Value :"+ multiplication);
+        System.out.println("Value :"+ percentage);
+
+        //< > <= >= != ==
+        boolean isTenLowerThanTwenty = 10 < 20;
+        boolean isTenGreaterThanTwenty = 10 > 20;
+        boolean isTenLowerOrEqualsThanTwenty = 10 <= 20;
+        boolean isTenGreaterOrEqualsThanTwenty = 10 >= 20;
+        boolean isTenDifferentThanTwenty = 10 != 20;
+        boolean isTenEqualsThanTwenty = 10 == 10;
+        System.out.println("isTenLowerThanTwenty: " + isTenLowerThanTwenty  );
+        System.out.println("isTenGreaterThanTwenty: " + isTenGreaterThanTwenty );
+        System.out.println("isTenLowerOrEqualsThanTwenty: " + isTenLowerOrEqualsThanTwenty );
+        System.out.println("isTenGreaterOrEqualsThanTwenty: " + isTenGreaterOrEqualsThanTwenty );
+        System.out.println("isTenDifferentThanTwenty: " + isTenDifferentThanTwenty );
+        System.out.println("isTenEqualsThanTwenty: " + isTenEqualsThanTwenty);
     }
 }
