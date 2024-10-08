74bf86e09ed3bf7102d3b0809e71aa3c20e326fa
franklinux <universomacroexpande@gmail.com>
2024-10-08 11:08:31 -0300

    primitive data

--- src/academy/devdojo/maratonajava/introduction/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introduction/Aula02TiposPrimitivos.java
@@ -2,7 +2,17 @@ package src.academy.devdojo.maratonajava.introduction;
 
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
-        int age = 10;
-        System.out.println("Age is: " + age + ".");
+        //int, double, flout, char, byte, short, long, boolean
+        int age = 18;
+        long bigNumber = 2004;
+        float floatPayment = 2500;
+        byte byteAge = 127;
+        short shortAge = 10;
+        boolean falsce = false;
+        char carctere = '\u0041';
+
+        System.out.println(falsce);
+        System.out.println("char" + carctere);
+        System.out.println("Age is: " + age + " years old.");
     }
 }
