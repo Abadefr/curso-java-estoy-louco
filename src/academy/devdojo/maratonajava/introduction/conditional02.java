d5bff98821575f85476d290d12622da4b287bb0a
franklinux <universomacroexpande@gmail.com>
2024-10-09 14:29:57 -0300

    if else part 2

---
+++ src/academy/devdojo/maratonajava/introduction/conditional02.java
@@ -0,0 +1,19 @@
+package src.academy.devdojo.maratonajava.introduction;
+
+public class conditional02 {
+    public static void main(String[] args) {
+        float salaryManeger = 5000;
+        float salaryDeveloper = 3500;
+        /*if (salaryManager > salaryDeveloper
+        if (salaryManager < salaryDeveloper
+        if (salaryManager >= salaryDeveloper
+        if (salaryManager <= salaryDeveloper
+        if (salaryManager != salaryDeveloper*/
+        if (salaryManager > salaryDeveloper) {
+            System.out.println("salary manager is higher:$" + salaryManager);
+        } else {
+            System.out.println("Developer's salary is lower than manager's:$" + salaryDeveloper);
+        }
+
+    }
+}
