75045c169f5e3821b7615fe1fe9db76b5b9b95c6
franklinux <universomacroexpande@gmail.com>
2024-10-09 11:58:56 -0300

    Age exercise in java 2

---
+++ src/academy/devdojo/maratonajava/introduction/iVariablesExercise02.java
@@ -0,0 +1,10 @@
+package src.academy.devdojo.maratonajava.introduction;
+
+public class iVariablesExercise02 {
+    public static void main(String[] args) {
+        double salary = 5450.89;
+        double taxPercentage = 0.1;
+        double taxAmoutFromSalary = salary * taxPercentage;
+        System.out.println(taxAmoutFromSalary);
+    }
+}
